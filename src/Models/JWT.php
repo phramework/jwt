06c3f62ecc9862238d71a43971e613c25cadd191
Xenophon Spafaridis <nohponex@gmail.com>
2015-11-20 23:53:42 +0200

    Test implementation

--- src/Models/JWT.php
+++ src/Models/JWT.php
@@ -19,14 +19,20 @@ namespace Phramework\JWT\Models;
 
 class JWT extends \Phramework\Models\Authentication
 {
+    protected $getUserByEmail = null;
+
     /**
      * Set the method that accepts email and returns a user object
      * containg a password
      * @param [type] $callable [description]
      */
-    public function setGetUserByEmail($callable)
+    public static function setGetUserByEmail($callable)
     {
+        if (!is_callable($callable)) {
+            throw new \Exception('Provided method is not callable');
+        }
 
+        self::$getUserByEmail = $callable;
     }
 
     /**
@@ -37,12 +43,93 @@ class JWT extends \Phramework\Models\Authentication
      */
     public static function check($params, $method, $headers)
     {
+        if (!isset($headers['Authorization'])) {
+            return false;
+        }
+
+        list($jwt) = sscanf($headers['Authorization'], 'Authorization: Bearer %s');
+
+        if (!$jwt) {
+            return false;
+        }
+
+        $secret    = \Phramework\Phramework::getSetting('jwt', 'secret');
+        $algorithm = \Phramework\Phramework::getSetting('jwt', 'algorithm');
+
+        try {
+
+            $token = JWT::decode($jwt, $secretKey, [$algorithm]);
+
+            return [];
+        } catch (Exception $e) {
+            /*
+             * the token was not able to be decoded.
+             * this is likely because the signature was not able to be verified (tampered token)
+             */
+//            header('HTTP/1.0 401 Unauthorized');
+            return false
+        }
+
         //read from token
-        return false;
+        //return false;
     }
 
     public static function authenticate($email, $password)
     {
-        //check if authenticated
+        if (!self::$getUserByEmail) {
+            throw new \Exception('getUserByEmail is not set');
+        }
+
+        $email = Validate::email($email);
+
+        $user = self::$getUserByEmail($email);
+
+        if (!$user) {
+            return false;
+        }
+
+        if (!password_verify($password, $user['password'])) {
+            return false;
+        }
+        
+        header('Content-type: application/json');
+
+        $secret     = \Phramework\Phramework::getSetting('jwt', 'secret');
+        $algorithm  = \Phramework\Phramework::getSetting('jwt', 'algorithm');
+
+        $serverName = \Phramework\Phramework::getSetting('jwt', 'server');
+
+        $tokenId    = base64_encode(mcrypt_create_iv(32));
+        $issuedAt   = time();
+        $notBefore  = $issuedAt + 10;  //Adding 10 seconds
+        $expire     = $notBefore + 60; // Adding 60 seconds
+
+        /*
+        * Create the token as an array
+        */
+        $data = [
+            'iat'  => $issuedAt,         // Issued at: time when the token was generated
+            'jti'  => $tokenId,          // Json Token Id: an unique identifier for the token
+            'iss'  => $serverName,       // Issuer
+            'nbf'  => $notBefore,        // Not before
+            'exp'  => $expire,           // Expire
+            'data' => [                  // Data related to the signer user
+                'user_id'  => $user['id'], // userid from the users table
+                'username' => $user['username'], // User name
+            ]
+        ];
+
+        $jwt = JWT::encode(
+            $data,      //Data to be encoded in the JWT
+            $secretKey, // The signing key
+            $algorithm  // Algorithm used to sign the token, see https://tools.ietf.org/html/draft-ietf-jose-json-web-algorithms-40#section-3
+        );
+
+        $unencodedArray = ['jwt' => $jwt];
+
+
+        echo json_encode($unencodedArray);
+
+        die();
     }
 }
